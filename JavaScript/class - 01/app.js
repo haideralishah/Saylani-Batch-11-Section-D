43e456cc92a4114c9b948a065c19e755bb34ac93
haideralishah <headeralishah@gmail.com>
2024-02-26 18:02:25 +0500

    JS started

---
+++ JavaScript/class - 01/app.js
@@ -0,0 +1,32 @@
+/*
+String ""  ''
+Number
+
+alert();
+
+*/
+
+// alert("1 Welcome Students!");
+
+// alert('Welcome Students!');
+
+// alert('Welcome Naveen!');
+
+// alert('Welcome Mubashir!');
+
+
+
+// alert('Quaid Azam Said: "There is no power on earth that can undo Pakistan."')
+
+
+// console.log("Hello World!");
+
+// console.log('My phone # 03059898769');
+
+console.log(5 + 5);
+console.log(5 - 2);
+console.log(5 * 2);
+console.log(10 / 2);
+console.log(10 ** 3);
+
+
