5a12fd897e71a13984ba4482d4f6ab13492a585f
mabdullahjs <mabdullah2037@outlook.com>
2024-02-28 15:59:08 +0500

    JS lab started

---
+++ JavaScript/labs/lab 1/app.js
@@ -0,0 +1,105 @@
+// alert("Haider Ali");
+// console.log(5+7+9-2*5);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// alert("Welcome to JS Land... \nHappy Coding!");
+// console.log("Welcome to JS Land... \nHappy Coding!");
+
+// document.write("Welcome to JS Land... <br /> Happy Coding!");
+// document.write("<br />")
+// document.write("<h1>Welcome Haider!</h1>");
+
+
+
+
+
+
+
+
+
+// let userName;
+// userName = "Haider";
+
+// console.log(userName);
+
+// userName = "Tuaha";
+
+// console.log(userName);
+
+
+
+
+
+// let userName, fatherName, rollNumber;
+
+// userName = "Haider";
+// fatherName = "Muhammad Shah Alam";
+// rollNumber = "2643";
+
+// console.log(userName);
+
+
+
+
+
+
+
+
+
+
+// let userName = "haider ali";
+// console.log(userName);
+
+// let userName = "haider ali", fatherName = 'Shah alam', rollNumber = "2563";
+
+
+
+
+
+
+
+
+// let userName = "Haider Ali";
+// let fatherName = "Shah Alam";
+// let rollNumber = "2643";
+
+// console.log(userName, fatherName, rollNumber);
+
+
+
+
+
+
+
+
+// let userName = "Haider Ali";
+// let greet = 'Eid Mubarak';
+// console.log(greet + " " + userName);
+
+// console.log("2" + "2");
+// console.log(2 + 2);
+
+
+
+
+
+
+
+
+
