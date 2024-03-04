98b81cae20f3af87896435001dae19c8dcb80764
haideralishah <headeralishah@gmail.com>
2024-03-04 18:00:09 +0500

    prompt learnt

---
+++ JavaScript/class - 02/app.js
@@ -0,0 +1,165 @@
+// alert('Hello World');
+// console.log("Hello JS");
+// document.write("Hello JS planet <br />");
+// document.write(1);
+
+// let studentName = "Akram";
+
+// console.log(studentName);
+// document.write("<h1>Welcome <span class='student-name'>" + studentName + "</span> to our class.</h1>");
+// document.write("<p>I am the <b>master</b> of my own fate.</p>");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let num1 = 2;
+// let num2 = 5;
+// let total = num1 ** num2;
+// console.log(total);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// console.log(31 % 5);
+// console.log(35 % 5);
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let num1 = 54986463161;
+
+// console.log(num1 % 2);
+
+
+
+
+
+
+
+
+// let userName = prompt("what is your name?"); //"haider"
+
+// document.write("Welcome " + userName + " to JS land.");
+
+
+
+
+
+
+// let num1 = +prompt("enter a number");
+// let num2 = +prompt("enter another number");
+// console.log(num1 + num2);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let userName = prompt('What is your name?');
+let num1 = +prompt('enter a number');
+let num2 = +prompt('enter another number');
+document.write('Welcome ' + userName);
+document.write("<br />");
+document.write("Your total is:" + (num1 + num2));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let dayCount = 7;
+
+// console.log(dayCount++); //7
+// console.log(++dayCount); //9
+// console.log(dayCount++); //9
+
+
+// let bullets = 9;
+// console.log(bullets--); //9
+// console.log(bullets--); //8
+// console.log(--bullets); //6
+
+
+
+
+
+// let num1 = 5;
+// let num2 = 9;
+// let sum = num1++ + ++num1 + num2++ - ++num2;
+//5      +  7     + 9      -   11
+// console.log(sum);
