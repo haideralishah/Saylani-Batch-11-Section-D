45ccca81b8ef62247fe29ea6e9825c547060d33a
haideralishah <headeralishah@gmail.com>
2024-03-06 16:00:35 +0500

    Str, num, prompt, math operations practice are done

---
+++ JavaScript/labs/lab 2/app.js
@@ -0,0 +1,79 @@
+// let institute = prompt("where do you study?");
+
+// document.write("Wow! " + institute + " is an awesome institute.");
+
+
+
+
+// let num1 = prompt('enter your lucky number');
+
+// console.log(num1 + 4);
+
+
+
+
+
+// let emailId = prompt('what is your email ID?');
+// let message = "My email address is " + emailId;
+
+// console.log(message);
+// alert(message);
+// document.write(message);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let str = 5;
+// let num = "5";
+// console.log(typeof str);
+// console.log(typeof num);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let birthYear = +prompt('enter your birth year');
+
+// document.write("My birth year is " + birthYear);
+// document.write('<br /> Data type of my declared variable is ' + typeof birthYear);
+
+
+
+
+
+
+let message = '"Haider" ordered "3" shirts from abc store of "500". His total bill is "1500"';
+
+document.write(message);
+
+
+
+
+
+
+
+
+
+
+
