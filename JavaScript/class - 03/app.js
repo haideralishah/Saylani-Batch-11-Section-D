cc8e6f8c069d0c8f26854332a20da26331d6b6c2
haideralishah <headeralishah@gmail.com>
2024-03-08 16:04:15 +0500

    conditions

---
+++ JavaScript/class - 03/app.js
@@ -0,0 +1,278 @@
+
+/*
+>
+<
+>=
+<=
+===
+!==
+*/
+
+
+
+
+
+
+
+
+
+// let table = +prompt('enter table name'); //4
+// let count = 1;
+
+// document.write(table + " x " + count + " = " + table * count++ + "<br />");
+
+// document.write(table + " x " + count + " = " + table * count++ + "<br />");
+// document.write(table + " x " + count + " = " + table * count++ + "<br />");
+// document.write(table + " x " + count + " = " + table * count++ + "<br />");
+// document.write(table + " x " + count + " = " + table * count++ + "<br />");
+// document.write(table + " x " + count + " = " + table * count++ + "<br />");
+// document.write(table + " x " + count + " = " + table * count++ + "<br />");
+// document.write(table + " x " + count + " = " + table * count++ + "<br />");
+// document.write(table + " x " + count + " = " + table * count++ + "<br />");
+// document.write(table + " x " + count + " = " + table * count++ + "<br />");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+boolean
+true
+false
+
+*/
+
+
+
+// let isEligible = confirm("are you under 15?");
+
+// console.log(isEligible);
+
+
+
+
+
+
+
+
+
+
+
+
+// let isMudassirShopOpen = confirm('Is Mudassir shop open?');
+
+// if (isMudassirShopOpen) {
+//     console.log("purchase lentils");
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let age = +prompt('what is your age?');
+
+// if (age < 15) {
+//     console.log("You are eligible for admission.");
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let domecile = prompt('In which city do you live?');
+
+// if (domecile === "karachi") {
+//     console.log("you are not eligible for govt jobs.");
+// }
+
+
+
+
+
+// let domecile = prompt('In which city do you live?');
+
+// if (domecile !== 'karachi') {
+//     console.log('Congrats! You are eligible for govt jobs.');
+// }
+
+
+
+
+
+// let age = +prompt('what is your age?');
+
+// if (age >= 15) {
+//     console.log("You are eligible for admission.");
+// }
+
+
+
+
+
+
+
+
+// let isShopOpen = confirm("Is Mudassir shop is open?");
+
+// if (isShopOpen) {
+//     console.log("purchase lentils");
+// }
+// else {
+//     console.log('purchase chicken');
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let age = +prompt('what is your age?');
+
+// if (age >= 15) {
+//     console.log('you are eligible for football club.');
+// }
+// else {
+//     console.log('you are eligible for Cricket club.');
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let cleanestCity = "karachi";
+let userCity = prompt("where do you live?");
+
+if (userCity === cleanestCity) {
+    document.write("Wow! You live in a cleanest city.");
+}
+else {
+    document.write("Oh no! You need to clean your city.");
+}
