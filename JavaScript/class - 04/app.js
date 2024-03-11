1dc4ead02aa2924921d4a621c75aef35efe9df22
haideralishah <headeralishah@gmail.com>
2024-03-11 15:53:48 +0500

    if else if else

---
+++ JavaScript/class - 04/app.js
@@ -0,0 +1,192 @@
+// let isMatriculate = confirm("are you Matriculate?");
+
+// if (isMatriculate) {
+//     document.write("Congrats! you are enrolled in SMIT");
+// }
+// else {
+//     document.write("Minimum Matriculate is required");
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let score = +prompt('what was your matric percentage?');
+// if (score >= 75) {
+//     document.write("Congrats! you are enrolled in SMIT");
+// }
+// else {
+//     document.write('minimum 75% is required');
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let shopName = prompt('which shop is open?');
+// if (shopName === "farhan") {
+//     console.log('bring french fries');
+// }
+// else if (shopName === 'almadina') {
+//     console.log('bring ande wala burger');
+// }
+// else if (shopName === "ejaaz") {
+//     console.log('bring biryani');
+// }
+// else {
+//     console.log('bring something to eat');
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let isMatriculate = confirm("are you Matriculate?");
+// let enteryTestScore = +prompt('what was your score?');
+
+// if (isMatriculate && enteryTestScore >= 60) {
+//     document.write("Congrats! you are enrolled in SMIT");
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let foodAvailable = prompt('which food is available?');
+
+// if (foodAvailable === "french fries" || foodAvailable === 'burger') {
+//     console.log("bring 1");
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// if (foodAvailable === "french fries" || foodAvailable === "burger") {
+//     console.log("bring 1");
+// }
+// else if (restaurantName === 'Kababjees' || restaurantName === 'foods inn') {
+//     console.log('let us visit resturants');
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let weight = +prompt("enter your weight");
+let bodyFat = +prompt("enter your body fat percentage");
+let sports = prompt('which sports do you play?');
+
+if ((weight > 300 && bodyFat <= 20) || sports === "Cricket") {
+    console.log('you are eligible for admission.');
+}
