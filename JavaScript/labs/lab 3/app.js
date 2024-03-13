f22e4a587c703157c4aba9505dcc7f7886f48829
haideralishah <headeralishah@gmail.com>
2024-03-13 15:01:24 +0500

    lab work for if else

---
+++ JavaScript/labs/lab 3/app.js
@@ -0,0 +1,146 @@
+// let city = prompt('what is your city name?');
+
+// if (city === 'Karachi') {
+//     console.log('welcome to the city of lights.');
+//     alert('welcome to the city of lights.');
+//     document.write('welcome to the city of lights.');
+// }
+
+
+
+
+
+
+
+
+// let city = prompt('what is your city name?');
+
+// if (city === 'Karachi') console.log('welcome to the city of lights.');
+// else console.log('Please come to our city');
+
+
+
+
+
+// let a = 4;
+// if (++a === 5) {
+//     alert("given condition for variable a is true");
+// }
+
+// let b = 82;
+// if (b++ === 83) {
+//     alert("given condition for variable b is true");
+// }
+
+
+
+
+// let c = 12;
+// if (c++ === 13) { //12
+//     alert("condition 1 is true");
+// }
+// if (c === 13) { // 13
+//     alert("condition 2 is true");
+// }
+// if (++c < 14) { // 14
+//     alert("condition 3 is true");
+// }
+// if (c === 14) { //14
+//     alert("condition 4 is true");
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var materialCost = 20000;
+// var laborCost = 2000;
+// var totalCost = materialCost + laborCost;
+// if (totalCost === laborCost + materialCost) {
+//     alert("The cost equals");
+// }
+
+
+
+
+
+// if ("car" < "cat") {
+//     alert("car is smaller than cat");
+// }
+
+
+// let percentage = +prompt('what was your score?');
+
+// if (percentage >= 80) {
+//     console.log('Excellant! you have got A1');
+// }
+// else if (percentage >= 70) {
+//     console.log('Good! you have got A');
+// }
+// else if (percentage >= 60) {
+//     console.log('You need to improve! you have got B');
+// }
+// else if (percentage < 60) {
+//     console.log('Moye Moye');
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let secreteNumber = 4;
+// let userPick = +prompt('pick a number from 1 to 5');
+// if (secreteNumber === userPick) {
+//     console.log('Bingo! correct answer.');
+// }
+// else if (secreteNumber === userPick - 1 || secreteNumber === userPick + 1) {
+//     console.log('You are very close.');
+// }
+
+
+
+
+
+
+
+
+
+
+// let num1 = +prompt('enter a number');
+
+// if(num1 % 3 === 0){
+
+// }
+
+if (num1 % 2 === 0) {
+
+}
+
+
+
+
+
+
