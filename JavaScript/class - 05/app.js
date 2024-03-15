5128b5ba7669abdb66b1a0dc55eaf00468a8d689
haideralishah <headeralishah@gmail.com>
2024-03-15 15:00:09 +0500

    array introduced

---
+++ JavaScript/class - 05/app.js
@@ -0,0 +1,198 @@
+// question# 15 sheet 12,13
+
+// let hour = prompt('what is the current hour?');
+// if (hour >= 6 && hour <= 9) {
+//     console.log('breakfast is served');
+// }
+// else if (hour >= 11 && hour <= 13) {
+//     console.log('lunch is served');
+// }
+// else if (hour >= 17 && hour <= 20) {
+//     console.log("dinner is served.");
+// }
+// else {
+//     console.log('sorry, please wait.');
+// }
+
+
+
+
+
+// question# 17 sheet 12,13
+
+// let chr = prompt('enter a character');
+// if (
+//     chr === "a" ||
+//     chr === "e" ||
+//     chr === "i" ||
+//     chr === "o" ||
+//     chr === "u"
+// ) {
+//     console.log('it is a vowel');
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let areYouGraduate = confirm("are you graduate?");
+
+// if (areYouGraduate) {
+//     let faculty = prompt('Which faculty you chose?');
+//     if (faculty === 'medical') {
+//         console.log('you are appointed as Doctor.');
+//     }
+//     else if (faculty === 'engineer') {
+//         console.log('you are appointed as engineer.');
+//     }
+//     else if (faculty === 'commerce') {
+//         let interest = prompt('are you interested in HR or Accounts?');
+//         if (interest === 'HR') {
+//             console.log('you are appointed as HR');
+//         }
+//         else if (interest === "accounts") {
+//             console.log('you are appointed as accountant');
+//         }
+
+//     }
+// }
+// else {
+//     console.log('you are appointed as peon.');
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let students = ["Naveen", "Mubashir", "Tauha", "Qetmeer", "Akram", "Naveen"];
+
+// console.log(students[4]);
+
+
+
+
+
+
+// let cars = ['Ferrari', "lamborghini", "Tesla"];
+// console.log(cars[1]);
+
+
+
+// let luckyNumbers = [5, 7, 9, 11, 15];
+
+// console.log(luckyNumbers[4]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let students = [];
+
+// students[0] = "Haider";
+// console.log(students);
+
+// students[1] = "Abdullah";
+// console.log(students);
+
+// students[3] = "Muzammil";
+// console.log(students);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let numbers = [5, 7, 13, 17, 19];
+
+// console.log(numbers.length);
+// console.log(numbers.indexOf(18));
+
+
+// let vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
+
+// let chr = prompt('enter a chr');
+
+// if (vowels.indexOf(chr) !== -1) {
+//     console.log('this is a vowel.');
+// }
+
+
+
+
+
+
+
+// Array.length;
+// Array.indexOf();
+// Array.includes();
+
+
+
+let vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
+
+let chr = prompt('enter a chr');
+
+if (vowels.includes(chr)) {
+    console.log('this is a vowel.');
+}
+
+
+
+
+
+
+
+
+
+
