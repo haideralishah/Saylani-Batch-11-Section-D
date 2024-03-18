79bf8dce7ddb8997ecd2ae3b17042290c429baee
haideralishah <headeralishah@gmail.com>
2024-03-18 14:40:57 +0500

    array methods

---
+++ JavaScript/class - 06/app.js
@@ -0,0 +1,172 @@
+/*
+array.length
+array.includes() // boolean
+array.indexOf() // index number or -1
+array.pop() // remove last index
+array.push() // add to last index
+array.shift()  // remove first index
+array.unshift() // add to first index
+array.splice()  // add or remove or both to targeted index
+
+
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let fruits = ["apple", "banana", "oranges"];
+
+// fruits[3] = "grapes";
+
+// console.log(fruits.length);
+// console.log(fruits);
+
+
+// let favFruit = prompt('what is your fav fruit?');
+// if (fruits.includes(favFruit)) {
+//     console.log("I have already bought your favorite fruit");
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+// let books = ["Deewan Ghalib", "Bale Jebreel", "Bahar Shariat", "Rich Dad Poor Dad"];
+
+// let userRequiredBook = prompt('Which book do you need?');
+
+// let bookIndex = books.indexOf(userRequiredBook);
+// if (bookIndex > -1) {
+//     console.log("Your required book index is " + bookIndex);
+// }
+// else {
+//     console.log('Sorry your required book is unavailable.');
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let fruits = ["apple", "banana", "oranges"];
+
+// fruits.pop();
+
+// fruits.push("mango", "kharbooza");
+
+// console.log(fruits);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let fruits = ["apple", "banana", "oranges"];
+
+// fruits.shift();
+
+// fruits.unshift("mango", "kharbooza");
+
+// console.log(fruits);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let fruits = ["apple", "banana", "oranges", "mango", "kharbooza"];
+
+// fruits.splice(2, 0, "grapes", "Tarbooz");
+
+// console.log(fruits);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
