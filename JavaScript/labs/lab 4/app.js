371aefd041c57dd18e1f1866cbef02d7da0e5084
usman-rizwan <usmanrizwan771@gmail.com>
2024-03-20 16:30:28 +0500

    arrays practice

---
+++ JavaScript/labs/lab 4/app.js
@@ -0,0 +1,83 @@
+// let cars = [];
+// let userName = prompt("what is your name?");
+// let mixxedArr = [true, 1, "haider", false, userName]
+
+
+
+
+// let arr1 = [1, 2, 3];
+// let arr2 = [4, 5, 6];
+
+// let arr3 = arr1.concat(arr2);
+// console.log(arr3);
+
+
+
+
+
+
+
+
+
+
+// let arr1 = [1, 2, 3];
+// let arr2 = [4, 5, 6];
+
+// let arr3 = [[1, 2, 3], [4, 5, 6]];
+// let arr3 = [arr1, arr2];
+// console.log(arr3)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// document.write("<h3>Qualifications:</h3>");
+// let qualifications = ["SSC", "HSC", "BCS", "BS", "BCOM", "MS", "M.Phil", "PhD"];
+// let index = 0;
+
+// document.write("<ol>");
+// document.write("<li>" + qualifications[index++] + "</li>");
+// document.write("<li>" + qualifications[index++] + "</li>");
+// document.write("<li>" + qualifications[index++] + "</li>");
+// document.write("<li>" + qualifications[index++] + "</li>");
+// document.write("<li>" + qualifications[index++] + "</li>");
+// document.write("<li>" + qualifications[index++] + "</li>");
+// document.write("<li>" + qualifications[index++] + "</li>");
+// document.write("<li>" + qualifications[index++] + "</li>");
+// document.write("</ol>");
+
+
+
+
+
+
+let qualifications = ["SSC", "HSC", "BCS", "BS", "BCOM", "MS", "M.Phil", "PhD"];
+
+let lastIndex = qualifications.length - 1
+
+qualifications[lastIndex]
+
+
+
+
+
+
+// let arr = [prompt(), prompt(), prompt()];
+
+// arr[0] = prompt();
+// arr[1] = prompt();
+
+
+
+
