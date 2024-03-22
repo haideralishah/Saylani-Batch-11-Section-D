678d8dc74055abf770a408f665a6a1e173dbdd5d
haideralishah <headeralishah@gmail.com>
2024-03-22 15:02:06 +0500

    string methods and arrays methods

---
+++ JavaScript/class - 07/app.js
@@ -0,0 +1,154 @@
+// let primeNum = [1, 2, 3, 5, 7, 11, 13, 17];
+
+// primeNum.splice(3, 2, 19, 23);
+// primeNum.splice(3, 3)
+
+// console.log(primeNum);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let num = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+// let favNum = num.slice(2, 7);
+
+// console.log(favNum, num);
+
+
+
+
+
+
+
+
+
+
+// let cricketers = ["Afridi", "Misbah", "Dhoni", "Yovraj", "Shoaib Akhtar", "Muhammad Asif", "Tendulkar", "Sehwag"];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let strArr = ["h", "a", "i", "d", "e", "r"];
+
+// console.log(strArr.reverse().join(""));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let firstName = 'haider ali shah';
+
+// console.log(firstName[7]);
+
+// console.log(firstName.length);
+
+// console.log(firstName.slice(7, 10));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let firstName = 'Haider Ali Shah';
+
+// console.log(firstName.toLowerCase());
+// console.log(firstName.toUpperCase());
+
+// if (str.toLowerCase() === 'Haider'.toLowerCase()) {
+
+// }
+
+
+
+
+
+
+
+
+
+
+
+// let cleanCity = "karachi";
+// let userCity = prompt("In which city do you live?");
+
+// if (userCity.toLowerCase() === cleanCity.toLowerCase()) {
+
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let userName = "hAIdER"; // Haider
+
+
+
+
+
+
+
+
