ecea42b259244045854b02a6e0848aed4fc58ec5
haideralishah <headeralishah@gmail.com>
2024-03-27 13:32:00 +0500

    loops

---
+++ JavaScript/class - 08/app.js
@@ -0,0 +1,149 @@
+// let fullName = ["haider", "ali", "shah"];
+
+// let fullNameStr = fullName.join(" ");
+// console.log(fullNameStr);
+
+
+
+
+
+
+// let userName = "haider ali shah";
+
+// let nameArr = userName.split("");
+// console.log(nameArr);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+starting point
+end point
+changes
+*/
+
+
+
+
+// for (let i = 0; i < 10; i++) {
+//     console.log(i);
+// }
+
+// console.log('hello world');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// for (let i = 100; i > 0; i--) {
+//     console.log("Current iteration:" + i);
+// }
+// for (let i = 0; i < 100; i = i + 2) {
+//     console.log("Current iteration:" + i);
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let table = +prompt("enter table name");
+
+// for (let i = 1; i <= 10; i++) {
+//  console.log(table + " x " + i + " = " + table * i);
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let students = ["aslam", 'akram', 'muzammil', 'abdullah'];
+
+for (let i = 0; i < students.length; i++) {
+    console.log('Hello ' + students[i])
+}
+
+
+
+
+
+
+
