84a18cae1cd0bf652040c39064b5dc3b9024a451
haideralishah <headeralishah@gmail.com>
2024-04-03 15:00:54 +0500

    Maths Methods

---
+++ JavaScript/labs/lab 6/app.js
@@ -0,0 +1,182 @@
+// // haider HAIDER hAIDeR Haider
+
+// let userName = prompt("What is your name?");// hAIDeR
+
+// let firstChr = userName[0];
+// firstChr = firstChr.toUpperCase();
+
+// let restChr = userName.slice(1);
+// restChr = restChr.toLowerCase();
+
+// console.log(firstChr + restChr);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let fruits = ["banana", "apple", "oranges", 'watermelon'];
+// console.log(fruits);
+
+// fruits.sort();
+
+// console.log(fruits);
+
+
+
+
+
+
+
+
+
+// let months = ["Jan", "Feb", "Mar", "Apr"];
+
+// let sorted = months.toSorted();
+// let reversed = months.toReversed();
+
+// console.log(months, sorted, reversed);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let num = 573.556;
+
+// let rounded = Math.round(num);
+
+// console.log(rounded);
+
+
+
+
+
+
+
+
+
+
+
+// let num1 = 1.9;
+// let num2 = 1.1; // 1
+
+// let ceiledVal = Math.ceil(num2);
+// let floorVal = Math.floor(num1);
+
+// console.log(ceiledVal, floorVal);
+
+
+
+
+
+
+
+
+
+// let a = Math.floor(-2.9999);
+// let b = Math.ceil(-4.99999);
+// console.log(a, b);
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let largest = Math.max(5, 7, 2, 3, 4, 7, 5, 6, 100, 15, 160);
+// let smallest = Math.min(5, 7, 2, 3, 4, 7, 5, 6, 100, 15, 160);
+
+// console.log(largest, smallest)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let num1 = [1, 2, 3];
+// let num2 = [4, 5, 6];
+// let nums = [...num1, ...num2];
+// console.log(nums);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let random = Math.ceil(Math.random() * 6);
+console.log(random);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
