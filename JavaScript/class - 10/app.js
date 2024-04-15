14c791801f15b87d061e3c663ce10365884628f8
haideralishah <headeralishah@gmail.com>
2024-04-15 15:58:14 +0500

    DOM introduction

---
+++ JavaScript/class - 10/app.js
@@ -0,0 +1,92 @@
+
+// let h1Element = document.querySelectorAll(".heading");
+
+// console.log(h1Element);
+// console.log(h1Element[0].innerHTML);
+// console.log(h1Element[1].innerHTML);
+
+// h1Element[0].innerHTML = "Hello <i>Mars</i>";
+
+
+
+
+
+
+// let imgElement = document.querySelectorAll("div>img");
+// console.log(imgElement[0].src);
+// console.log(imgElement[0].className);
+
+// imgElement[0].src = "images/shoaib.webp";
+// imgElement[0].className += " shoaib";
+
+
+
+
+
+
+
+
+
+// let cityElement = document.querySelectorAll('input');
+// console.log(cityElement[0].value);
+
+// cityElement[0].value = "Karachi";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let h1Elements = document.querySelectorAll("h2");
+// let h1Element = document.querySelector("h2");
+
+// console.log(h1Elements);
+// console.log(h1Element);
+
+
+
+
+
+
+
+
+
+let h2Elements = document.querySelectorAll("h2");
+h2Elements[1].innerHTML = "Hello Kotlin";
+
+let imgEl = document.querySelectorAll("img");
+imgEl[0].src = "images/shoaib.webp";
+imgEl[0].className += ' shoaib';
+
+let cityEl = document.querySelectorAll('input');
+cityEl[0].value = "Karachi"
+
+
+let aElement = document.querySelectorAll('a');
+aElement[0].href = "https://www.youtube.com/c/jsguru";
+aElement[0].innerHTML = "YouTube";
+
+
+
+
