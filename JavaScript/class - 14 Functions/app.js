3d3fbf2001061cce2dcb1a07b4254e12779c9e5b
haideralishah <headeralishah@gmail.com>
2024-04-29 16:05:58 +0500

    functions return concept

--- JavaScript/class - 14 Functions/app.js
+++ JavaScript/class - 14 Functions/app.js
@@ -196,14 +196,159 @@
 
 
 
-function dateDiff(dob) {
-    let today = new Date();
-    let diff = (today - dob) / 1000 / 60 / 60 / 24 / 365;
-    return Math.floor(diff);
+// function dateDiff(dob) {
+//     let today = new Date();
+//     let diff = (today - dob) / 1000 / 60 / 60 / 24 / 365;
+//     return Math.floor(diff);
+// }
+
+// let myAge = dateDiff(new Date("October 4, 1991"));
+
+// let studentAge = dateDiff(new Date("November 25, 1997"));
+
+// console.log(myAge, studentAge);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function findDivision(percentage) {
+//     let division;
+//     if (percentage >= 60) {
+//         division = "1st Division";
+//     }
+//     else if (percentage < 60 && percentage >= 45) {
+//         division = "2nd Division";
+//     }
+//     else {
+//         division = 'Failed';
+//     }
+//     return division;
+// }
+
+// let students = ["ahmed", "abdullah", "muzammil"];
+// let marks = [50, 55, 70];
+
+// let tBody = document.querySelectorAll('tbody');
+
+// for (let i = 0; i < students.length; i++) {
+//     let division = findDivision(marks[2]);
+
+//     tBody[0].innerHTML += `
+//     <tr>
+//         <td>${students[i]}</td>
+//         <td>${marks[i]}</td>
+//         <td>${division}</td>
+//     </tr>
+//     `
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// L * W * H
+
+
+// function findVolume(l, w, h) {
+//     //data conversion into meters
+//     let lInMeter = l / 39.3700787;
+//     let wInMeter = w / 39.3700787;
+//     let hInMeter = h / 39.3700787;
+//     let volume = hInMeter * wInMeter * lInMeter;
+//     return volume;
+// }
+
+// let item = ["remote", "laptop", "mobile"];
+// // data available in inches
+// let widths = [20, 40, 25];
+// let height = [15, 30, 45];
+// let length = [10, 15, 30];
+
+// for (let i = 0; i < item.length; i++) {
+//     let volume = findVolume(length[i], widths[i], height[i]);
+//     console.log(`${item[i]} volume is: ${volume}`);
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Jan Birth
+//June  Anniversary
+//Nov Job Anniversary
+function greetUser(monthName) {
+    if (monthName === "Jan") {
+        return "Happy Birthday";
+    }
+    else if (monthName === "Jun") {
+        return "Happy Wedding Anniversary";
+    }
+    else if (monthName === "Nov") {
+        return "Congrats! You have completed 2 years in our organisation.";
+    }
 }
 
-let myAge = dateDiff(new Date("October 4, 1991"));
+let greetMsg = greetUser("Nov");
 
-let studentAge = dateDiff(new Date("November 25, 1997"));
+console.log(greetMsg);
 
-console.log(myAge, studentAge);
+let h1El = document.querySelectorAll('h1');
+h1El[0].innerHTML = greetMsg;
