3512327f7cda541f3e9c52b0e4274555719d07d4
haideralishah <headeralishah@gmail.com>
2024-05-22 15:58:15 +0500

    todo edit functionality

--- JavaScript/labs/lab 11/app.js
+++ JavaScript/labs/lab 11/app.js
@@ -1,12 +1,12 @@
-let todoInput = document.querySelectorAll('.new-todo');
+let todoInput = document.querySelectorAll('.new-todo')[0];
 let todoListDiv = document.querySelectorAll('.todo-list');
 let allTodos = [];
 
 function add() {
-    let todoVal = todoInput[0].value;
+    let todoVal = todoInput.value;
     allTodos.push(todoVal);
     printAllTodos();
-    todoInput[0].value = '';
+    todoInput.value = '';
 }
 
 function printAllTodos() {
@@ -22,12 +22,50 @@ function printAllTodos() {
     }
 }
 
-function editTodo(index) {
-    console.log('edit', index);
+function deleteTodo(index) {
+    allTodos.splice(index, 1);
+    printAllTodos();
 }
 
 
-function deleteTodo(index) {
-    allTodos.splice(index, 1);    
+let editTodoDiv = document.querySelectorAll('#edit-todo-container')[0];
+let addTodoDiv = document.querySelectorAll('#add-new-todo-container')[0];
+let editTodoInput = document.querySelectorAll('.edit-todo')[0];
+let editIndex;
+function editTodo(index) {
+    if (isEditing) {
+        displayError();
+    }
+    else {
+        editIndex = index;
+        toggleTodoForm();
+        editTodoInput.value = allTodos[index];
+    }
+}
+
+function upadateTodo() {
+    toggleTodoForm();
+    allTodos.splice(editIndex, 1, editTodoInput.value);
     printAllTodos();
+}
+
+let isEditing = false;
+function toggleTodoForm() {
+    if (!isEditing) {
+        editTodoDiv.className = "";
+        addTodoDiv.className += " hide";
+    }
+    else {
+        editTodoDiv.className += " hide";
+        addTodoDiv.className = "";
+    }
+    isEditing = !isEditing;
+}
+
+let errorPEl = document.querySelectorAll('.error-msg')[0];
+function displayError() {
+    errorPEl.innerHTML = "First save edited todo.";
+    setTimeout(function () {
+        errorPEl.innerHTML = "";
+    }, 3000);
 }
