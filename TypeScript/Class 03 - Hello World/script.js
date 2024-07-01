5c841b87388e9386602ae6cebf8d493e125a2920
haideralishah <headeralishah@gmail.com>
2024-07-01 17:42:45 +0500

    typescript enums touples casting

---
+++ TypeScript/Class 03 - Hello World/script.js
@@ -0,0 +1,54 @@
+"use strict";
+// npx tsc --init
+// let basicColors: [string, string, string];
+// basicColors = ["red", "green", "blue"];
+// let basicColors2: [string, string, string] = ["red", "green", "blue"];
+// let basicColors3: string[] = [];
+// let ourTuple: [boolean, string, number];
+// ourTuple = [false, 'Coding God was mistaken', 10];
+// const graph: [x: number, y: number] = [55.2, 41.3];
+// const pending = "pending";
+// const confirmed = 'confirmed';
+// const delivered = "delivered";
+// const shipped = "shipped";
+// enum OrderStatus {
+//     PENDING,
+//     CONFIRMED,
+//     SHIPPED,
+//     DELIVERED
+// }
+// console.log(OrderStatus.CONFIRMED);
+// enum HttpStatus {
+//     success = 200,
+//     BadRequest  = 400,
+//     myMistake = 500
+// }
+// console.log(HttpStatus.success);
+// console.log(HttpStatus[200])
+// console.log(HttpStatus.BadRequest);
+// console.log(HttpStatus.myMistake);
+// enum MathsConst {
+//     PI = 3.14,
+//     randomNum = Math.random()
+// }
+// console.log(MathsConst.PI);
+// console.log(MathsConst.randomNum);
+// console.log(MathsConst.randomNum);
+// enum Province {
+//     Sindh = "Sindh",
+//     Punjab = "Punjab",
+//     Balochistan = "Balochistan",
+//     KP = "KP",
+//     GilgitBaltistan = "GilgitBaltistan"
+// }
+// console.log(Province.Sindh);
+// console.log(Province.KP);
+// for (let key in Province) {
+//     console.log(Province[key]);
+// }
+// let abc = prompt('what is your name');
+// let userName: string = abc as string;
+// let user2: string = <string>abc;
+// function add(a: number, b: number): number {
+//     return a + b;
+// }
---
+++ TypeScript/Class 04 - Hello World/script.js
@@ -0,0 +1,2 @@
+import { userName } from './utils/sum.js';
+console.log(userName);
