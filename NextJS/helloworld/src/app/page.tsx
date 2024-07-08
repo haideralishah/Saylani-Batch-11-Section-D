0bb8d703a752089c12e47225cc9b80ca68bf3d1f
haideralishah <headeralishah@gmail.com>
2024-07-08 16:01:40 +0500

    nextjs hello world

---
+++ NextJS/helloworld/src/app/page.tsx
@@ -0,0 +1,10 @@
+
+
+export default function Home(){
+  return (
+    <div>
+      <h1>Hello world</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur enim nulla eius, officia amet provident animi, adipisci iure ut aut culpa pariatur doloribus vel sint ad, harum mollitia! Explicabo, officia.</p>
+    </div>
+  )
+}
