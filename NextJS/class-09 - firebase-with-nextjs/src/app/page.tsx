6bfe4ef04413efded932189ff2acfb3580cbc593
haideralishah <headeralishah@gmail.com>
2024-09-18 13:32:46 +0500

    firebase auth example

--- NextJS/class-09 - firebase-with-nextjs/src/app/page.tsx
+++ NextJS/class-09 - firebase-with-nextjs/src/app/page.tsx
@@ -7,7 +7,7 @@ import { useState } from "react";
 export default function Home() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-
+  const [user, setUser] = useState({});
 
   return (
     <>
